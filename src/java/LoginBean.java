7e8543f3b3c0c17fe817663927a06832f09b3fcd
Sebastian <sebbeharry@gmail.com>
2017-01-18 16:04:17 +0100

    Added LoginBean and form in login.xhtml

---
+++ src/java/LoginBean.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author Sebastian
+ */
+@Named(value = "loginBean")
+@Dependent
+public class LoginBean {
+    
+    private String email;
+    private String password;
+
+    /**
+     * Creates a new instance of LoginBean
+     */
+    public LoginBean() {
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
