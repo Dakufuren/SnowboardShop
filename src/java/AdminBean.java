e62ae71f9040019711b5fd1f9f5e256d066ddcfe
Lieutenant-Dan <sebbeharry@gmail.com>
2017-01-19 17:45:05 +0100

    Added adminpage and bean for future use

---
+++ src/java/AdminBean.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author Sebastian
+ */
+@Named(value = "adminBean")
+@Dependent
+public class AdminBean {
+
+    /**
+     * Creates a new instance of AdminBean
+     */
+    public AdminBean() {
+    }
+    
+}
