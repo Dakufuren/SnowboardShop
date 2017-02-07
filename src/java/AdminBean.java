f21ef9e6edb8d21de0d32e12bd90f30ce2111fd0
Lieutenant-Dan <sebbeharry@gmail.com>
2017-02-07 18:08:45 +0100

    Added methods for checking if user is admin or not.

--- src/java/AdminBean.java
+++ src/java/AdminBean.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 
+import Hibernate.HibernateHelper;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
 
@@ -14,11 +15,29 @@ import javax.enterprise.context.Dependent;
 @Named(value = "adminBean")
 @Dependent
 public class AdminBean {
+    
+    private String email;
+    HibernateHelper hh = new HibernateHelper();
+    DataStorage ds = new DataStorage();
 
     /**
      * Creates a new instance of AdminBean
      */
     public AdminBean() {
+    }
+    
+    public void checkAdmin(){
+        
+        
+        email = ds.getEmail();
+        Boolean checkIfAdmin=hh.isAdmin(email);
+        
+        if(checkIfAdmin == true){
+            System.out.println("YOU ARE LOGGED IN AS ADMIN" +  "" +email + "" + checkIfAdmin);
+        }
+        else
+            System.out.println("YOU ARE NOT LOGGED IN AS ADMIN" +  "" +email + "" + checkIfAdmin);
+        
     }
     
 }
