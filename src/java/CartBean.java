051dd14bf865111c0e5d5ba0cdf6011d1cbe4a60
Albin Åberg <albin.aberg@outlook.com>
2017-02-08 16:36:44 +0100

    Fixed some problems in with the cart

--- src/java/CartBean.java
+++ src/java/CartBean.java
@@ -24,59 +24,71 @@ import javax.faces.context.FacesContext;
 @ManagedBean(name = "cartBean")
 @SessionScoped
 public class CartBean {
-    
+
     private List<Snowboard> cartList;
     private List<Snowboard> snowList;
     private DataStorage ds;
     private String selectedItem;
+    private int totalSum;
+    private boolean calcAlreadyRun = false;
+    private boolean changeOnList = false;
 
     /**
      * Creates a new instance of CartBean
      */
-    
     @PostConstruct
-    public void init(){
-         cartList = new ArrayList<>();
-         ds = new DataStorage();
+    public void init() {
+        cartList = new ArrayList<>();
+        ds = new DataStorage();
     }
 
     public CartBean() {
-        
-    }
-    
-    public void delFromCart(){
-        for (Snowboard s : cartList) {
-            if(s.getProductName().equals(selectedItem)){
-                cartList.remove(s);
-                System.out.println("Removed!!!!");
-            }
-        }
-    }
-    
-    public void clearCart(){
-        for (Snowboard s : cartList) {
-            if(s.getProductName().equals(selectedItem)){
-                cartList.remove(s);
-                System.out.println("Removed!!!!");
-            }
-        }
-    }
-    
-    public void addToCart(){
-        FacesContext fc = FacesContext.getCurrentInstance();
-        Map<String,String> params = fc.getExternalContext().getRequestParameterMap();
-	int tempID = Integer.parseInt(params.get("id"));
-        
-        snowList = ds.getSnowlist();
 
+    }
+
+    public void delFromCart(Snowboard s) {
+        cartList.remove(s);
+        changeOnList = true;
+    }
+
+    public void clearCart() {
+        System.out.println("CLEARING CARTT!!!");
+        cartList.clear();
+        changeOnList = true;
+    }
+
+    public void addToCart() {
+        FacesContext fc = FacesContext.getCurrentInstance();
+        Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
+        int tempID = Integer.parseInt(params.get("id"));
+
+        snowList = ds.getSnowlist();
         cartList.add(snowList.get(tempID - 1));
 
-        System.out.println(getCartList().size());
         System.out.println(cartList.size());
+        changeOnList = true;
     }
 
-    public void checkOut(){
+    public void checkOut() {
 
+    }
+    
+    public int calcTotalSum() {
+        if (changeOnList == true || calcAlreadyRun == false) {
+            totalSum = 0;
+            calcAlreadyRun = false;
+
+            for (Snowboard s : cartList) {
+                if (calcAlreadyRun == false) {
+                    totalSum = totalSum + s.getPrice();
+                }
+            }
+        }
+
+        calcAlreadyRun = true;
+        changeOnList = false;
+
+        return totalSum;
     }
 
     /**
